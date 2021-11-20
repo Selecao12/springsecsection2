82c886b11b5063ff41aa5c79c8a3db850aa63242
vitalik <witalikalive@gmail.com>
2021-11-20 22:46:49 +0500

    13. Modifying the code as per our custom requirements - finished

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -3,15 +3,28 @@ package com.eazybytes.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.AuthorizedUrl;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * /myAccount - Secured
+     * /myBalance - Secured
+     * /myLoans - Secured
+     * /myCard - Secured
+     * /notices - Not Secured
+     * /contact - Not Secured
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests((requests) -> {
-            ((AuthorizedUrl)requests.anyRequest()).authenticated();
+            requests
+                    .antMatchers("/myAccount").authenticated()
+                    .antMatchers("/myBalance").authenticated()
+                    .antMatchers("/myLoans").authenticated()
+                    .antMatchers("/myCard").authenticated()
+                    .antMatchers("/notices").permitAll()
+                    .antMatchers("/contact").permitAll();
         });
         http.formLogin();
         http.httpBasic();
